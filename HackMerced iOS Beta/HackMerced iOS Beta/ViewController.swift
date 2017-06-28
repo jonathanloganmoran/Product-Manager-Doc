ee23f77e986023c48ec349e475f8b5139ce4889e
jonathanloganmoran <jmoran23@ucmerced.edu>
2017-06-27 23:03:36 -0700

    Initial Project Release

---
+++ HackMerced iOS Beta/HackMerced iOS Beta/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HackMerced iOS Beta
+//
+//  Created by Jonathan Moran on 6/27/17.
+//  Copyright © 2017 HackMerced. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
